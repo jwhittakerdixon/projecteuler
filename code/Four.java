8973ecc6ac552d6a44572da1802e36cd967b7722
J Whittaker-Dixon <jwhittakerdixon@gmail.com>
2017-08-12 20:48:43 +0100

    Update Four.java

--- code/Four.java
+++ code/Four.java
@@ -1,37 +1,54 @@
 import java.io.*;
 
-public class Four {
+public class Three {
 
 	public static void main(String[] args) {
 
-		int a = 999;
-		int b = 999;
-
+		long x = 600851475143L;
+		long y = 0;
 
 		while (true) {
 
-			if (isPalindrome(a*b)){break;}
-			a=a-1;
+			y = reduce(x);
+			if (x==y) {break;}
+			x=y;
 
 		}
 
-		System.out.println(a*b);
-
+		System.out.println(y);
+		
 	}
 
-	public static Boolean isPalindrome(int x) {
+	public static long reduce(long x) {     //takes int, divides out smallest divisor
 
-		String n = new Integer(x).toString();
-		String r = "";
+		long a = sqrt(x);
+		Boolean prime = true;
 
-		int length = n.length();
- 
-      		for ( int i = length - 1; i >= 0; i-- ) {
-         		r = r + n.charAt(i);
- 		}
-
-		if (r.equals(n)){return true;}else{return false;}
+		for (long i = 2; i <= sqrt(x); i++) {   //finds smallest divisor
+			if (x % i == 0){
+				a=i;
+				prime=false;
+				break;
 	
+			}
+		}
+		
+		if (prime){return x;}          //if no smallest divisor found, we're done
+		return x/a;
+	
+
 	}
+
+	public static int sqrt(long x) {
+		
+		int y = 0;
+		for (int i = 1 << 15; i != 0; i >>>= 1) {
+			y |= i;
+			if (y > 46340 || y * y > x)
+				y ^= i;
+		}
+		return y;
+	}
+
 
 }
